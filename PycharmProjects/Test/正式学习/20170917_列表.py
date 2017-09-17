1c8c7b966bf11337dc72b4c4821c50486c0b8d23
mly <249966292@qq.com>
2017-09-17 22:37:22 +0800

    提交python练习代码

---
+++ PycharmProjects/Test/正式学习/20170917_列表.py
@@ -0,0 +1,49 @@
+#coding=utf-8
+
+list = ["zhangsan","lisi","wangwu","zhaoliu"]
+
+
+
+print list
+
+
+list.append("chaochao")
+print list
+
+
+list.pop()
+print list
+
+list.extend(["mly","chaochao"])
+print list
+
+#给上边的列表添加年龄
+
+age = [12,13,14,15,16,17,18]
+
+print list[3]
+#列表的数据可以类型不同
+temp = []
+i = 0
+
+while i < len(list):
+    temp.append(list[i])
+    temp.append(age[i])
+    i+=1
+else:
+    print temp
+
+#for 循环遍历
+for j in temp:
+    print j
+
+#while 遍历
+k = 0
+while k < len(temp):
+    print temp[k]
+    k+=1;
+#测试插入，其他数据后移
+#temp.insert(5,"测试位")
+print temp
+
+

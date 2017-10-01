41e9bc1fdaff32c04503d0073ca59bd13a8c0011
mly <249966292@qq.com>
2017-10-01 18:41:54 +0800

    测试

--- PycharmProjects/Test/练习/20170906_字符串.py
+++ PycharmProjects/Test/练习/20170906_字符串.py
@@ -3,37 +3,37 @@
 str = "i love y"
 
 # 输出整个字符串
-print str
+print (str)
 
 # 从第二位开始，输出整个字符串
-print str[2]
+print (str[2])
 
 # 从第二位开始，输出三位字符串
-print str[2:5]
+print (str[2:5])
 
 # 输出两边字符串
-print str*2
+print (str*2)
 
 # 字符串拼接
-print str+"chaochao"
+print (str+"chaochao")
 
 #字符串格式化
 
 
-print "我是一个 %s，今年%d岁" % ('中国人',21)
-print "My name is %s and weight is %d kg!" % ('Zara', 21)
+print ("我是一个 %s，今年%d岁" % ('中国人',21))
+print ("My name is %s and weight is %d kg!" % ('Zara', 21))
 
 
 
 #三引号
 
-print '''123
+print ('''123
 456
-789'''
+789''')
 
 #unicode
 
-print u'Hello\u0020World !'
+print (u'Hello\u0020World !')
 
 
 
